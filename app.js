cb5ff41549308e1df600e82b20c0d38c25b3d4f7
FrozzenIce <pstggames80@gmail.com>
2026-02-01 21:01:24 +0545

    Project Structure & codes

--- app.js
+++ app.js
@@ -2,21 +2,33 @@ require("dotenv").config()
 // Async errors
 
 const express = require("express")
-const app = require(express())
+const app = express()
 
-const notFoundMiddleware = require("./middleware/error-handler")
+const connectDB = require("./DB/connectDB")
+
+const notFoundMiddleware = require("./middleware/not-found")
 const errorHandlerMiddleware = require("./middleware/error-handler")
-const PORT = 3000
 
 app.use(express.json())
-app.use("/api/v1/")
+// app.use("/api/v1/")
 
 //routes
 app.get("/", (req, res) => {
-    res.send('<h1>Store API</h1><a href="/api/v1/products>Products route</a>')
+    res.send('<h1>Store API</h1><a href="/api/v1/products">Products route</a>')
 })
 
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
-const start = () => {
-    app.listen(PORT, `Server is listening on PORT ${PORT}`)
+const PORT = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_DB_URI)
+        app.listen(PORT, console.log(`Server is listening on PORT ${PORT}`))
+    } catch (error) {
+        console.log(error);
+    }
 }
+
+start()
